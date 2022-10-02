9155f187a4629baca04ab413c1fdd1e1ce224a4e
Tejas Shelke <tejasshelke040402@gmail.com>
2022-10-03 00:26:05 +0530

    SPRING CRUD USING REST

---
+++ src/main/java/com/tej/example/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package com.tej.example.cruddemo.dao;
+
+import java.util.List;
+
+import com.tej.example.cruddemo.entity.Employee;
+
+public interface EmployeeDAO {
+	
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+}
---
+++ src/main/java/com/tej/example/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,48 @@
+package com.tej.example.cruddemo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tej.example.cruddemo.dao.EmployeeDAO;
+import com.tej.example.cruddemo.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+	
+	private EmployeeDAO employeeDAO;
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+		employeeDAO = theEmployeeDAO;
+		
+	}
+	
+
+	@Override
+	@Transactional
+	public List<Employee> findAll() {
+		// TODO Auto-generated method stub
+		return employeeDAO.findAll();
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		// TODO Auto-generated method stub
+		return employeeDAO.findById(theId);
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		employeeDAO.save(theEmployee);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(int theId) {
+		employeeDAO.deleteById(theId);
+	}
+
+}
